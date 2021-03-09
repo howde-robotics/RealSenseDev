82c46ee5dd56acac5e8b73f6cda8dc1b33590ba4
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-03-09 08:37:06 -0500

    added results of calibrated output to npz file

--- opencv_calibration.py
+++ opencv_calibration.py
@@ -14,19 +14,21 @@ import glob
 #     print("hey")
 
 # termination criteria
+cbRow = 8
+cbCol = 6
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((cbRow*cbCol,3), np.float32)
+objp[:,:2] = np.mgrid[0:cbCol,0:cbRow].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.jpg')
+images = glob.glob('images/*.png')
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (8,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (cbRow,cbCol), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         print("Pattern Found")
@@ -35,10 +37,15 @@ for fname in images:
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (8,6), corners2, ret)
+        cv.drawChessboardCorners(img, (cbRow,cbCol), corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(0)
 
 cv.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+print(mtx, dist, rvecs, tvecs)
+
+# save the calibration for later use
+np.savez("calibration_output", intrinsics=mtx, distortion_coeffecients=dist, ext_rotation=rvecs, ext_translation=tvecs)
