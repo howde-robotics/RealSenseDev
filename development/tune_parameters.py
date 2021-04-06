0f39ecb56ae2688032f87f6810fdca349cf6022a
Jacqueline Liao <liaoj@mit.edu>
2021-04-05 22:23:31 -0700

    adding ros node and messages

--- development/tune_parameters.py
+++ development/tune_parameters.py
@@ -20,13 +20,20 @@ if __name__ == "__main__":
     sensor = pipeline.get_active_profile().get_device().query_sensors()[1]
 
     # Set the exposure anytime during the operation
-    sensor.set_option(rs.option.exposure, 10.000)
+    # sensor.set_option(rs.option.exposure, 10.000)
 
     # Only the first frame is captured in manual exposure mode with an unknown exposure value!
     for _ in range(10):
         frames = pipeline.wait_for_frames()
         depth = np.asanyarray(frames.get_depth_frame().get_data())
-        # print(depth)
+        print(depth)
         plt.imshow(depth)
         plt.show()
+
+        bridge = CvBridge()
+        depth_image = bridge.imgmsg_to_cv2(depth, desired_encoding="passthrough")
+        depth_array = np.array(depth_image, dtype=np.float32)
+        center_idx = np.array(depth_array.shape) / 2
+        print ('center depth:', depth_array[center_idx[0], center_idx[1]])
+
         pipeline.stop()
